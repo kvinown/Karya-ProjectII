f1bc330548ab634a71d3ed2c6aa3fa98eb4ed1d5
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2022-11-04 12:56:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+let button = document.getElementById('button');
 let stars = document.getElementById('stars');
 let meteorid = document.getElementById('meteorid');
 let rocket = document.getElementById('rocket');
 let text = document.getElementById('text');
+let desc = document.getElementById('desc');
+let kepin =document.getElementById('kepin');
+
+alert("Jika anda menggunakan HP, gunakan desktop site.");
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     stars.style.left = value * 0.25 + 'px';
-    rocket.style.top = value * -0.5 + 'px';
+    rocket.style.top = value * -1 + 'px';
     text.style.marginBottom = value * 1 + 'px';
+    desc.style.marginBottom = value * 1 + 'px';
+    kepin.style.marginBottom = value * 1 + 'px';
+    button.style.marginBottom = value * 1 + 'px';
 });
